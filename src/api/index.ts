475faac95f3630b9a57dcf744673d6a6fcfeec59
avaqado <gunkin.art@yandex.ru>
2020-04-10 17:33:55 +0300

    Add api

---
+++ src/api/index.ts
@@ -0,0 +1,58 @@
+import Axios from "axios";
+
+interface RegisterData {
+  username: string;
+  password: string;
+  email: string;
+}
+
+interface FilterData {
+  filter: string;
+}
+
+interface LoginData {
+  email: string;
+  password: string;
+}
+
+interface TransactionData {
+  name: string;
+  amount: number;
+}
+
+const BASE_URL = "http://193.124.114.46:3001/";
+const USERS = "/users";
+const LOGIN = "/sessions/create";
+const USER_PROFILE = "/api/protected/user-info";
+const FILTER_USERS = '"/api/protected/users/list';
+const TRANSACTIONS = "/api/protected/transactions";
+
+const api = Axios.create({
+  timeout: 1000,
+  baseURL: BASE_URL,
+  headers: {
+    "Content-type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+const token = {
+  headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+};
+
+const storeToken = ({ data }: any) => {
+  localStorage.setItem("token", data["id_token"]);
+  return data;
+};
+
+export const userApi = {
+  info: () => api.get(USER_PROFILE, token),
+  filter: (data: FilterData) => api.post(FILTER_USERS, data, token),
+  login: (data: LoginData) => api.post(LOGIN, data).then(storeToken),
+  register: (data: RegisterData) => api.post(USERS, data).then(storeToken),
+};
+
+export const transactionsApi = {
+  create: (data: TransactionData) => api.post(TRANSACTIONS, data, token),
+  all: () => api.get(TRANSACTIONS, token),
+};
